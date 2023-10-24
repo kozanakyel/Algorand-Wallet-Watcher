f87a1f3795a3c14697dad68629ac65346b676d72
kozanakyel <kozanakyel@gmail.com>
2023-10-24 16:32:47 +0300

    add get account info to mainnetwalletservice

--- src/algo_wallet_watcher/Infrastructure/services/mainnet_wallet_service.py
+++ src/algo_wallet_watcher/Infrastructure/services/mainnet_wallet_service.py
@@ -1,4 +1,5 @@
 from algo_wallet_watcher.Infrastructure.config import MAINNET_ACCOUNT_API
+import requests
 
 
 class MainnetWalletService:
@@ -14,7 +15,18 @@ class MainnetWalletService:
             cls._instance.main_uri = MAINNET_ACCOUNT_API
         return cls._instance
 
-
+    def get_account_info(self, algorand_address):
+        url = f'{self.main_uri}{algorand_address}'
+        headers = {
+            'accept': 'application/json'
+        }
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.json()  
+        else:
+            return {'error': f'{response.status_code} - {response.text}'}
+    
+    
 if __name__ == "__main__":
     k = MainnetWalletService()
-    print(k.main_uri)
+    print(k.get_account_info('jvadsfkfbasl'))
