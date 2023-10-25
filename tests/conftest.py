8f126e8c4c95d5497f771e11571aeb87f58396d8
kozanakyel <kozanakyel@gmail.com>
2023-10-25 18:40:03 +0300

    add integration test for orm mappers

---
+++ tests/conftest.py
@@ -0,0 +1,48 @@
+# pylint: disable=redefined-outer-name
+import time
+from pathlib import Path
+
+import pytest
+import requests
+from requests.exceptions import ConnectionError
+from sqlalchemy.exc import OperationalError
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, clear_mappers
+
+from algo_wallet_watcher.Infrastructure.orm_mapper.orm import metadata, start_mappers
+from algo_wallet_watcher.Infrastructure import config
+
+
+@pytest.fixture
+def in_memory_db():
+    engine = create_engine("sqlite:///:memory:")
+    metadata.create_all(engine)
+    return engine
+
+
+@pytest.fixture
+def session_factory(in_memory_db):
+    start_mappers()
+    yield sessionmaker(bind=in_memory_db)
+    clear_mappers()
+    
+    
+@pytest.fixture
+def session(session_factory):
+    return session_factory()
+
+def wait_for_webapp_to_come_up():
+    deadline = time.time() + 10
+    url = config.get_api_url()
+    while time.time() < deadline:
+        try:
+            return requests.get(url)
+        except ConnectionError:
+            time.sleep(0.5)
+    pytest.fail("API never came up")
+    
+@pytest.fixture
+def restart_api():
+    (Path(__file__).parent / "../src/algo_wallet_watcher/api/endpoints/app.py").touch()
+    time.sleep(0.5)
+    wait_for_webapp_to_come_up()
