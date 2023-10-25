6197058fbc5c0a381d1f3010a653822c4e273859
kozanakyel <kozanakyel@gmail.com>
2023-10-26 00:49:11 +0300

    editedt pytest module for new feature

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,12 +5,36 @@ from pathlib import Path
 import pytest
 import requests
 from requests.exceptions import ConnectionError
-from sqlalchemy.exc import OperationalError
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, clear_mappers
 
 from algo_wallet_watcher.Infrastructure.orm_mapper.orm import metadata, start_mappers
 from algo_wallet_watcher.Infrastructure import config
+
+from tenacity import retry, stop_after_delay
+
+pytest.register_assert_rewrite("tests.e2e.test_api")
+
+@pytest.fixture
+def sqlite_session_factory(in_memory_sqlite_db):
+    yield sessionmaker(bind=in_memory_sqlite_db)
+
+
+@pytest.fixture
+def mappers():
+    start_mappers()
+    yield
+    clear_mappers()
+
+
+@retry(stop=stop_after_delay(10))
+def wait_for_postgres_to_come_up(engine):
+    return engine.connect()
+
+
+@retry(stop=stop_after_delay(10))
+def wait_for_webapp_to_come_up():
+    return requests.get(config.get_api_url())
 
 
 @pytest.fixture
